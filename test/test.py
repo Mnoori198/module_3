4410be6c4377f88a0748ca3f09f42f363fca75aa
mqasi <mnoori@dmacc.edu>
2019-09-21 15:13:16 -0500

    initial commit

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ from main_page import average_score
 
 class MyTestCase(unittest.TestCase):
     def test_average(self):
-        with mock.patch('builtins.input', side_effect=([85, 90, 95])):
+        with mock.patch('builtins.input', side_effect=([90, 85, 95])):
             assert average_score.average() == 90
     # assert average_score.average() == 90
 
